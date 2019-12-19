5534b6e09cc48fd40acfdbb2a5357a6cec606113
Andrei <vlasovproger@gmail.com>
2019-12-19 15:45:10 +0300

    added DNDA project

---
+++ dnda/precache-manifest.d9aa044e907271046d018124b3a353ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f911a43c7aba583c46c9c649bf72e70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f466d97662e358f5141c",
+    "url": "/static/css/main.42b6877b.chunk.css"
+  },
+  {
+    "revision": "0c85c41b963a92dee83f",
+    "url": "/static/js/2.f4814a8b.chunk.js"
+  },
+  {
+    "revision": "f466d97662e358f5141c",
+    "url": "/static/js/main.2d5d6cb7.chunk.js"
+  },
+  {
+    "revision": "aa5c8cdb7c9c288b7516",
+    "url": "/static/js/runtime-main.6966677e.js"
+  }
+]);
